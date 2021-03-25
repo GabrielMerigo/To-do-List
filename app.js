0ab52ba556afcae8f3f9a28da9fae676c9be1d5b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-03-24 21:22:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,47 +2,63 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const searchInput = document.querySelector('.form-search input')
 
-formAddTodo.addEventListener('submit', event => {
+const insertTodoIntoDOM = event => {
   event.preventDefault()
 
   const input = event.target.add.value.trim()
+  const HTMLtemplate = `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${input}</span>
+      <i class="far fa-trash-alt delete"></i>
+  </li>
+  `;
+
   if (input.length) {
-    todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${input}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>
-    `
+    todosContainer.innerHTML += HTMLtemplate;
     event.target.reset()
   }
-})
-
-todosContainer.addEventListener('click', removeTodo)
-
-searchInput.addEventListener('input', event => {
-  const inputValue = event.target.value.trim().toLowerCase()
-
-  Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(li => {
-      li.classList.remove('d-block')
-      li.classList.add('hidden')
-    })
-
-  Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(li => {
-      li.classList.remove('hidden')
-      li.classList.add('d-block')
-    })
-})
-
+}
 
 const removeTodo = event => {
-  const thereIsClassDelete = event.target.className.includes('delete');
+  const classTarget = event.target.className;
+  const liRemoved = event.target.parentElement
 
-  if (thereIsClassDelete) {
-    const li = document.querySelector('.delete');
-    todosContainer.removeChild(li.parentElement)
+  if (classTarget.includes('delete')) {
+    todosContainer.removeChild(liRemoved)
   }
 }
+
+const removeTodoInputSearch = (todosChildren, inputValue) => {
+  Array.from(todosChildren)
+    .filter(todo => {
+      const includesValueInput = todo.textContent
+        .toLowerCase()
+        .includes(inputValue)
+
+      return includesValueInput
+    })
+    .forEach(li => li.classList.remove('hidden'))
+}
+
+const addTodoInputSearch = (todosChildren, inputValue) => {
+  Array.from(todosChildren)
+  .filter(todo => {
+    const notIncludesValueInput = todo.textContent
+      .toLowerCase()
+      .includes(inputValue)
+
+    return !notIncludesValueInput
+  }).forEach(li => li.classList.add('hidden'))
+} 
+
+const validationSearchInputValue = event => {
+  const inputValue = event.target.value.trim().toLowerCase()
+  const todosChildren = todosContainer.children;
+
+  removeTodoInputSearch(todosChildren, inputValue)
+  addTodoInputSearch(todosChildren, inputValue)
+}
+
+formAddTodo.addEventListener('submit', insertTodoIntoDOM)
+todosContainer.addEventListener('click', removeTodo)
+searchInput.addEventListener('input', validationSearchInputValue)
